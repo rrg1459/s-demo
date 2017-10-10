8ba338b5b647f3b540b237f4df1158ca64e13826
Rafael <rrg1459@hotmail.com>
2017-10-10 09:22:25 -0400

    commit para sms-demo 11

--- app/models/recarga.rb
+++ app/models/recarga.rb
@@ -2,6 +2,6 @@ class Recarga < ApplicationRecord
   belongs_to :user
 
   validates :monto_bs,      presence: { message: "en blanco" }, 
-             length: {minimum: 4, :message => "debe tener por lo menos 4 dígitos"}
+             length: {minimum: 3, :message => "debe tener por lo menos 3 dígitos"}
 
 end
